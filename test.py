1b0c65566148178c886748aa12eb77f5a87409d0
tomoldenbeuving <tom.oldenbeuving@gmail.com>
2023-04-26 14:41:40 +0200

    dataframe is uitgelezen

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-print("hello world")
-print('hello Tom')
+import pandas as pd
+import numpy as np
+
+rho_staal = 7.85E3
+rho_water = 1.025E3
+g = 9.811
+
+df = pd.read_excel("IP.xlsx")
+
+p = df.iloc[42:64,1]*rho_water*g
