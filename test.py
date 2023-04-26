e5e78f6539efd74c92f721bfb7d65efc5d980f1f
tomoldenbeuving <tom.oldenbeuving@gmail.com>
2023-04-26 14:58:51 +0200

    excel

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ rho_staal = 7.85E3
 rho_water = 1.025E3
 g = 9.811
 
-df = pd.read_excel("https://tud365-my.sharepoint.com/personal/toldenbeuving_tudelft_nl/Documents/IP.xlsx")
+df = pd.read_excel("IP.xlsx")
 
 p = df.iloc[42:64,1]*rho_water*g
 
 G = -df.iloc[97:119,2]*rho_staal*g
 
 q = p+G
+
 
