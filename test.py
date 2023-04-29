51c1913eb1f2ca576cf943974c37f69d8a5289ca
BartvNuland <b.j.t.vannuland@student.tudelft.nl>
2023-04-29 16:39:35 +0200

    Plots aangemaakt, ook begonnen aan I uitlezen

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from scipy import integrate, interpolate
 import matplotlib.pyplot as plt
 
 rho_staal = 7.85E3
+E_staal=210000E6
 rho_water = 1.025E3
 g = 9.811
 df = pd.read_excel("IP.xlsx")
@@ -30,19 +31,77 @@ x_G = np.append(nul,x_G)
 x_G = np.append(x_G,eind)
 G_func = interpolate.interp1d(x_G,G)
 
+I = df.iloc[97:119,6]
+I = np.append(nul,I)
+I = np.append(I,nul)
+x_I = df.iloc[97:119,0]
+x_I = np.append(nul,x_I)
+x_I = np.append(x_I,eind)
+I_func = interpolate.interp1d(x_I,I)
+
 x = np.arange(0.0,Loa,0.05)
 
 
 p = p_func(x)
 G = G_func(x)
+index=[np.arange(0,1)]
+I=np.delete(I_func(x),index)
 
 q= p+G
 
 V = integrate.cumtrapz(x,q,initial=0)
+M = integrate.cumtrapz(x,V,initial=0)
+#phi=(integrate.cumtrapz(np.delete(x,index),np.delete(M,index),initial=0))/(E_staal*I)
 
-def plot(x,y):
-    plt.plot(x,y)
+
+
+
+
+
+def plotp():
+    plt.plot(x,p)
+    plt.xlabel('L.O.A. [m]')
+    plt.ylabel('Opwaartsekracht (p) [N]')
+    plt.title('Opwaartsekracht uitgezet tegen de totale lengte')
     plt.grid()
     plt.show()
+    return plotp
+
+def plotG():
+    plt.plot(x,G)
+    plt.xlabel('L.O.A. [m]')
+    plt.ylabel('Zwaartekracht (G) [N]')
+    plt.title('Zwaartekracht uitgezet tegen de totale lengte')
+    plt.grid()
+    plt.show()
+    return plotG
+
+def plotq():
+    plt.plot(x,q)
+    plt.xlabel('L.O.A. [m]')
+    plt.ylabel('Nettobelasting (q) [N]')
+    plt.title('Nettobelasting uitgezet tegen de totale lengte')
+    plt.grid()
+    plt.show()
+    return plotq
+    
+def dwarskrachtlijn():
+    plt.plot(x,V)
+    plt.xlabel('L.O.A. [m]')
+    plt.ylabel('Dwarskracht (V) [N]')
+    plt.title('Dwarskracht uitgezet tegen de totale lengte')
+    plt.grid()
+    plt.show()
+    return dwarskrachtlijn
+
+def momentlijn():
+    plt.plot(x,M)
+    plt.xlabel('L.O.A. [m]')
+    plt.ylabel('Moment (M) [Nm]')
+    plt.title('Intern moment uitgezet tegen de totale lengte')
+    plt.grid()
+    plt.show()
+    return momentlijn
+
 
 
