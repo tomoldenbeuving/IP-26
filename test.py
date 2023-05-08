c16514a68eeed0e07b146dd5b9d1b1cce646f65c
jkfwensink <jiswiwensink11@gmail.com>
2023-05-08 11:02:45 +0200

    Co-authored-by: tomoldenbeuving <tomoldenbeuving@users.noreply.github.com>

--- test.py
+++ test.py
@@ -14,37 +14,23 @@ nul = np.zeros(1)
 Loa= df.iloc[0,1]
 eind = np.array([Loa])
 
-p = df.iloc[43:64,1]*rho_water*g
-p = np.append(nul,p)
-p = np.append(p,nul)
-x_p = df.iloc[43:64,0]
-x_p = np.append(nul,x_p)
-x_p = np.append(x_p,eind)
+
+
+
+p = df.iloc[42:64,1]*rho_water*g
+x_p = df.iloc[42:64,0]
 p_func = interpolate.interp1d(x_p,p)
 
 
 
-G = -df.iloc[98:119,2]*rho_staal*g
-G = np.append(nul,G)
-G = np.append(G,nul)
-x_G = df.iloc[98:119,0]
-x_G = np.append(nul,x_G)
-x_G = np.append(x_G,eind)
+G = -df.iloc[97:119,2]*rho_staal*g
+x_G = df.iloc[97:119,0]
 G_func = interpolate.interp1d(x_G,G)
 
-I = df.iloc[98:119,6]
-I = np.append(nul,I)
-I = np.append(I,nul)
-x_I = df.iloc[98:119,0]
-x_I = np.append(nul,x_I)
-x_I = np.append(x_I,eind)
+I = df.iloc[97:119,6]
+x_I = df.iloc[97:119,0]
+
 I_func = interpolate.interp1d(x_I,I)
-
-
-x = np.arange(0.0,Loa,0.5)
-
-
-p = p_func(x)
 G = G_func(x)
 I= I_func(x)
 
