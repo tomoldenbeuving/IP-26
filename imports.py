04acae168b49024e141e6149f7aed9c0141a410d
tlievaart <t.j.lievaart@student.tudelft.nl>
2023-05-25 12:40:19 +0200

    VB terug gevoegd

--- imports.py
+++ imports.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_excel(".\chonker\chonker.xlsx",'last')
+df = pd.read_excel("IP.xlsx",'VB schip van Goris')
 df = df.round(4)
 
-df_varend = pd.read_excel(".\chonker\chonker.xlsx",'varend')
+df_varend = pd.read_excel("IP.xlsx",'VB varend')
 df_varend = df_varend.round(4)
 
-df_leeg = pd.read_excel(".\chonker\chonker.xlsx",'leeg')
+df_leeg = pd.read_excel("IP",'VB leeg')
 df_leeg = df_leeg.round(4)
 
 tp_factor = 1
