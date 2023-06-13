9f6ecb257df757fbd8d4199157a628f40cfc89bc
jkfwensink <jiswiwensink11@gmail.com>
2023-06-13 11:26:53 +0200

    sa

--- Stabiliteitvarend.py
+++ Stabiliteitvarend.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy import integrate, interpolate
 import matplotlib.pyplot as plt
-from imports import df_varend, tp_factor,n,Cw,Ch,atiers,a
+from imports import df_varend, tp_factor,n,Cw,Ch,atiers,a,abay_varend,arij_varend,atiers_varend
 
 
 rho_staal = 7.85E3
@@ -97,11 +97,12 @@ KG = df_varend.iloc[21,3]
 gewichtschip=displacement*rho_water
 displacement1=(gewichtschip+Cw*n)/rho_water
 BM_t = It_x/displacement1
-KGcont_v=H+(Ch*atiers/2)
+KGcont_v=H+(Ch*atiers_varend/2)
 KGtank_v=df_varend.iloc[33,3]
 
 KG_nieuw= (KG*G_punt/g+KGcont_v*n*Cw+KGtank_v*volumetank*rho_water)/(G_punt/g+n*Cw+volumetank*rho_water)
-
+I_water=df_varend.iloc[38,1]
+gg1=I_water/displacement
 GM_t_v = KB + BM_t - KG_nieuw 
 
 
